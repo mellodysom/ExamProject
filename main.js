8b6fad301e776fcac70adbb718eb4c2890d3dd8a
phbr11ac <phbr11ac@student.cbs.dk>
2019-03-05 11:21:57 +0100

    Random code

--- main.js
+++ main.js
@@ -1,75 +1,128 @@
-// This program can calculate how much you will get paid after tax cuts if you enter salary, fradrag and tax percent
+// Class for construction users
 
-// Adding functionality to round down to nearest ten
-function roundDown(num, precision) {
-    precision = Math.pow(10, precision)
-    return Math.floor(num * precision) / precision }
-// Adding functionality to round up to nearest ten
-function roundUp(num, precision) {
-    precision = Math.pow(10, precision)
-    return Math.ceil(num * precision) / precision }
-// Constants
-const am = 0.08;
-const atp = [94.65, 63.1, 31.55, 0];
-// This should be obtained from the user
-var salary = 29600;
-var fradrag = 5043;
-var taxPercent = 0.36;
-var hoursWorked = 160;
-//  The calculations
-// Find amount of ATP paid
-if (hoursWorked > 117) {
-    n = 0; }
-else if (hoursWorked > 78) {
-    n = 1; }
-else if (hoursWorked > 39) {
-    n = 2; }
-else {n = 3;}
+class User {
+  constructor(id, username, password) {
+      this.id = id;
+      this.username = username;
+      this.password = password;  
+  } 
+} 
 
-amBidrag = roundDown( (salary - atp[n]) * am, -1);
-skattepligtigt = roundDown(salary-atp[n]-amBidrag-fradrag,-1);
-let taxPaid = roundUp(skattepligtigt * taxPercent,0);
-
-let paidOut = salary - atp[n] - amBidrag - taxPaid;
-
-console.log(paidOut); 
-
-
-/* THE FOLLOWING CODE IS NOT WORKING
-function test(salary, fradrag, taxPercent, hoursWorked) {
-    if (hoursWorked > 117) {
-        n = 0; }
-    else if (hoursWorked > 78) {
-        n = 1; }
-    else if (hoursWorked > 39) {
-        n = 2; }
-    else {n = 3;}
-    amBidrag = roundDown( (salary - atp[n]) * am, -1);
-skattepligtigt = roundDown(salary-atp[n]-amBidrag-fradrag,-1);
-let taxPaid = roundUp(skattepligtigt * taxPercent,0);
-
-var paidOut = salary - atp[n] - amBidrag - taxPaid;
-return paidOut;
-console.log(paidOut);
+class Admin extends User {
+  constructor(department, classes) {
+    this.department = department;
+    this.classes = classes;
+  }
 }
 
-test(29600, 5043, 0.36, 160)
-
-console.log(paidOut)
-
-
-
-// THIS IS NOT WORKING EITHER
-
-// Function to input the data needed for the calculations
-var myButton = document.querySelector('button');
-
-function inputData() {
-    var salary = prompt('Please enter your monthly salary.');
-    var fradrag = prompt('Please enter your monthly fradrag.')
-    var taxPercent = prompt('Please enter your tax percent.')
+class Students extends User {
+  constructor(dateOfStart,mastersProgram) {
+    this.dateOfStart = dateOfStart;
+    this.mastersProgram = mastersProgram
+  }
 }
 
-myButton.onclick = function() {
-    inputData();
-}*/
+// Class for construction electives
+
+class Electives {
+  constructor(id, cbsid, name) {
+    this.id = id;
+    this.cbsid = cbsid;
+    this.name = name;
+  }
+}
+
+var users = [];
+
+users.push(new User(1, "phbr11ac@student.cbs.dk", "123456"))
+
+
+
+
+/*
+
+
+
+
+var users = [{username: "Philip", password: "Braun", id: 1},{username:"Mellody", password: "Som", id: 2}]
+var count = 0
+
+// Function for adding users
+
+
+
+
+
+  
+
+adduser.onclick = function () {
+var username = prompt("Username");
+var password = prompt("Password");
+var id = prompt("id");
+
+users.push({username: username, password: password, id: id})
+}
+
+// Function for checking if user is known
+submit.onclick = function() {
+    var inputUsername = document.getElementById("username").value;
+    var inputPassword = document.getElementById("password").value;
+  
+    if (inputPassword.length < 1 || inputUsername.length < 1) {
+        alert("You should input something");
+        return false;
+    }
+    if (count == 3) {
+        alert("You have used all your tries")
+        document.getElementById("username").disabled=true;
+        document.getElementById("password").disabled=true;
+    }
+    for (let i = 0; i < users.length; i ++) {
+        if (inputPassword == users[i].password && inputUsername == users[i].username) {
+            document.getElementById("LoginResult").innerText = "You have signed in";    
+            return true;
+        }
+    }
+    alert("Password is wrong");
+    count ++
+
+    return false;
+}   
+
+
+elective1.onclick = function () {
+    var rating = prompt("Give a rating from 1 to 5");
+}
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+/*
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+
+  // FUNCTIONS TO MAKE THE LOGIN FUNCTION WORK
+
+// RANDOMS STUFF FROM ONLINE
+
+document.getElementById('date').innerHTML = new Date().toDateString();
+*/
